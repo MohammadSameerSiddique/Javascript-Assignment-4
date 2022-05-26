f20f9fd7f04d142414f3517fab9195e05dd1d2cc
MohammadSameerSiddique <siddiquesameer483@gmail.com>
2022-05-26 20:54:02 +0100

    work accomplish

---
+++ app.js
@@ -0,0 +1,72 @@
+        // Chapter 26-30
+        //Math Methods
+
+// 1. Write a program that takes a positive integer from user &
+//  display the following in your browser.
+//  a. number
+//  b. round off value of the number
+//  c. floor value of the number
+//  d. ceil value of the number.  
+
+// var num = 3.4523;
+
+// document.write("Number :"+" "+num+"</br>")
+// document.write("Round off :"+" "+Math.round(num)+"</br>")
+// document.write("Floor :"+" "+Math.floor(num)+"</br>")
+// document.write("Ceil :"+" "+Math.ceil(num)+"</br>")
+
+// 2. Write a program that takes a negative floating points from user &
+//  display the following in your browser.
+//  a. number
+//  b. round off value of the number
+//  c. floor value of the number
+//  d. ceil value of the number. 
+
+
+// var num = -3.6934;
+
+// document.write("Number :"+" "+num+"</br>")
+// document.write("Round off :"+" "+Math.round(num)+"</br>")
+// document.write("Floor :"+" "+Math.floor(num)+"</br>")
+// document.write("Ceil :"+" "+Math.ceil(num)+"</br>")
+
+// 3. Write a program that displays the absolute value of a number.
+//  E.g. absolute value of -4 is 4 & absolute value of 5 is 5
+
+// var a = -5;
+// var b =Math.abs(a)
+// document.write("The absolute value of"+" "+a+" "+"is"+" "+b)
+
+// 4. Write a program that simulates a dice using random() method of JS Math class.
+//  Display the value of dice in your browser.
+
+// var a = Math.floor(Math.random()*6)+1;
+// var b = Math.floor(Math.random()*6)+1;
+// document.write("Random dice value :"+a+"</br>"+"Random dice value :"+b)
+
+// 5. Write a program that simulates a coin toss using random() method of JS Math class.
+//  Display the value of coin in your browser.
+
+// var a = Math.floor(Math.random()*2)+1;
+// var b = Math.floor(Math.random()*2)+1;
+// document.write("Random coin value : Heads"+" "+a+"</br>"+"Random coin value : Tails"+" "+b)
+
+// 6. Write a program that shows a random number between 1 and 100 in your browser.
+
+// var a = Math.floor(Math.random()*100)+1;
+// document.write("Random number between 1 to 100 :"+" "+a)
+
+
+// 7. Write a program that stores a random secret number from 1 to 10 in a variable.
+//  Ask the user to input a number between 1 and 10.
+//  If the user input equals the secret number,
+//  congratulate the user.
+
+// var a = prompt("Enter number between 1 to 10");
+// var b = Math.floor(Math.random()*10) +1 ;
+// if(a == b){
+//     alert("Congratulation Match Your Number")
+// }else{
+//     alert("Please Try again!")
+// }
+
